43cc0b0859d27a225fefb86e0371eba587fcf07c
Salypse <84153483+Salypse@users.noreply.github.com>
2026-03-20 14:19:44 -0500

    Add styling to shop now button in homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,7 @@
 import { Link, useOutletContext } from "react-router";
 import { ProductSpotlight } from "./ProductSpotlight";
 import { BestSellers } from "./BestSellers";
+import styles from "../styles/homepage.module.css";
 
 export function Homepage() {
    const { productData } = useOutletContext();
@@ -8,7 +9,9 @@ export function Homepage() {
    return (
       <>
          <ProductSpotlight productData={productData} />
-         <Link to="/shop">SHOP NOW!</Link>
+         <Link to="/shop" className={styles.link}>
+            SHOP NOW!
+         </Link>
          <BestSellers productData={productData} />
       </>
    );

8a1928dd5a53a10c9006cc166a04dc0f599b4531
Salypse <84153483+Salypse@users.noreply.github.com>
2026-03-20 13:49:03 -0500

    Add styling to ProductSpotlight index buttons

--- src/components/ProductSpotlight.jsx
+++ src/components/ProductSpotlight.jsx
@@ -5,6 +5,7 @@ import {
 } from "../helper/productSpotlight";
 import { useEffect, useState } from "react";
 import { SpotlightCard } from "./SpotlightCard";
+import styles from "../styles/productSpotlight.module.css";
 
 export function ProductSpotlight(props) {
    const { productData } = props;
@@ -28,15 +29,16 @@ export function ProductSpotlight(props) {
    }, [spotlightProducts]);
 
    return (
-      <section className="spotlight">
+      <section className={styles["product-spotlight"]}>
          <h2>FEATURED</h2>
-         <div className="product-spotlight">
+         <div>
             <button
                onClick={() =>
                   setCurrentIndex((prevIndex) =>
                      decreaseIndex(prevIndex, spotlightProducts.length),
                   )
                }
+               className={`${styles["spotlight-button"]} ${styles["left"]}`}
             >
                {"<"}
             </button>
@@ -51,6 +53,7 @@ export function ProductSpotlight(props) {
                      increaseIndex(prevIndex, spotlightProducts.length),
                   )
                }
+               className={`${styles["spotlight-button"]} ${styles["right"]}`}
             >
                {">"}
             </button>

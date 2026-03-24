e3a4b4ef35d446b59ca1a2c32e289e7c713bd4e9
Salypse <84153483+Salypse@users.noreply.github.com>
2026-03-24 13:44:19 -0500

    Add styling to filter component and update it to be toggleable

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,47 +1,58 @@
 import { getCategories, filterData } from "../helper/filter";
 import { useRef } from "react";
+import styles from "../styles/filter.module.css";
+import closeIcon from "../assets/close-icon.svg";
 
 export function Filter(props) {
-   const { productData, setFilteredItems } = props;
+   const { productData, setFilteredItems, setIsActive } = props;
    const categories = getCategories(productData);
    const formRef = useRef(null);
 
    return (
       <>
-         <form ref={formRef}>
-            <legend>Filter Products</legend>
-            <ul>
-               {categories.map((category) => (
-                  <li key={category}>
-                     <div>
-                        <input
-                           type="radio"
-                           id={`filter-${category}`}
-                           name={`filter-option`}
-                           onClick={() =>
-                              setFilteredItems(
-                                 filterData(productData, category),
-                              )
-                           }
-                        ></input>
-                        <label htmlFor={`filter-${category}`}>
-                           {category.charAt(0).toUpperCase() +
-                              category.slice(1)}
-                        </label>
-                     </div>
-                  </li>
-               ))}
-               <button
-                  onClick={(event) => [
-                     event.preventDefault(),
-                     setFilteredItems(productData),
-                     formRef.current.reset(),
-                  ]}
-               >
-                  Clear Filter
-               </button>
-            </ul>
-         </form>
+         <div className={styles["filter"]}>
+            <button
+               className={styles["icon"]}
+               onClick={() => setIsActive((prev) => !prev)}
+            >
+               <img src={closeIcon} />
+            </button>
+            <form ref={formRef} className={styles["form"]}>
+               <legend>Filter Products</legend>
+               <ul className={styles["form-options"]}>
+                  {categories.map((category) => (
+                     <li key={category}>
+                        <div>
+                           <input
+                              type="radio"
+                              id={`filter-${category}`}
+                              name={`filter-option`}
+                              onClick={() =>
+                                 setFilteredItems(
+                                    filterData(productData, category),
+                                 )
+                              }
+                           ></input>
+                           <label htmlFor={`filter-${category}`}>
+                              {category.charAt(0).toUpperCase() +
+                                 category.slice(1)}
+                           </label>
+                        </div>
+                     </li>
+                  ))}
+                  <button
+                     className={styles["btn"]}
+                     onClick={(event) => [
+                        event.preventDefault(),
+                        setFilteredItems(productData),
+                        formRef.current.reset(),
+                     ]}
+                  >
+                     Clear Filter
+                  </button>
+               </ul>
+            </form>
+         </div>
       </>
    );
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,32 +3,48 @@ import { ShopCard } from "./ShopCard";
 import { Filter } from "./Filter";
 import { useState } from "react";
 import styles from "../styles/shop.module.css";
+import filterStyles from "../styles/filter.module.css";
+import filterIcon from "../assets/filter.svg";
 
 export function Shop() {
    const { productData, cartData, setCartData } = useOutletContext();
    const [filteredItems, setFilteredItems] = useState(productData);
+   const [isActive, setIsActive] = useState(false);
 
    return (
       <>
-         <aside className="filter">
-            <Filter
-               productData={productData}
-               setFilteredItems={setFilteredItems}
-            />
-         </aside>
-         <section>
-            <ul className={styles.shop}>
-               {filteredItems.map((product) => (
-                  <li key={product.id} className={styles["shop-product"]}>
-                     <ShopCard
-                        data={product}
-                        cartData={cartData}
-                        setCartData={setCartData}
-                     />
-                  </li>
-               ))}
-            </ul>
-         </section>
+         {isActive && (
+            <aside className="filter">
+               <Filter
+                  productData={productData}
+                  setFilteredItems={setFilteredItems}
+                  isActive={isActive}
+                  setIsActive={setIsActive}
+               />
+            </aside>
+         )}
+
+         {!isActive && (
+            <section>
+               <button
+                  className={filterStyles["icon"]}
+                  onClick={() => setIsActive((prev) => !prev)}
+               >
+                  <img src={filterIcon} />
+               </button>
+               <ul className={styles.shop}>
+                  {filteredItems.map((product) => (
+                     <li key={product.id} className={styles["shop-product"]}>
+                        <ShopCard
+                           data={product}
+                           cartData={cartData}
+                           setCartData={setCartData}
+                        />
+                     </li>
+                  ))}
+               </ul>
+            </section>
+         )}
       </>
    );
 }

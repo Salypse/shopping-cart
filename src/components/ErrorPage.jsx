ae480d368613966ba89340ce667c2118f8488e82
Salypse <84153483+Salypse@users.noreply.github.com>
2026-03-27 11:18:28 -0500

    Add styling to ErrorPage component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router";
+import styles from "../styles/errorPage.module.css";
 
 export function ErrorPage() {
    return (
-      <div>
+      <div className={styles["error-page"]}>
          <h1>This page doesn't exist</h1>
          <Link to="/">Home Page</Link>
       </div>

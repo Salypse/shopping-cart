cb64957e3d4a555af4da32dc7c7d6a99cd98ac98
Salypse <84153483+Salypse@users.noreply.github.com>
2026-03-27 10:46:16 -0500

    Fix shopcard not having cart quantity as default input value

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import { addToCart } from "../helper/addToCart";
 import styles from "../styles/shopCard.module.css";
 
 export function ShopCard(props) {
    const { data, cartData, setCartData } = props;
-   const [quantity, setQuantity] = useState(0);
+   const cartProduct = cartData.find((product) => product.id === data.id);
+   const initialQuantity = cartProduct ? cartProduct.quantity : 0;
 
    return (
       <div className={styles["shop-card"]}>
@@ -22,8 +22,10 @@ export function ShopCard(props) {
                <button
                   type="button"
                   className={styles["quantity-btn"]}
-                  onClick={() => {
-                     setQuantity((prev) => (prev - 1 > 0 ? prev - 1 : 0));
+                  onClick={(event) => {
+                     const input = event.currentTarget.nextSibling;
+                     input.value =
+                        input.value - 1 > 0 ? Number(input.value) - 1 : 0;
                   }}
                >
                   -
@@ -32,14 +34,15 @@ export function ShopCard(props) {
                   type="number"
                   name="quantity"
                   min={1}
-                  defaultValue={quantity > 0 ? quantity : ""}
+                  defaultValue={initialQuantity > 0 ? initialQuantity : ""}
                   required
                ></input>
                <button
                   type="button"
                   className={styles["quantity-btn"]}
-                  onClick={() => {
-                     setQuantity((prev) => prev + 1);
+                  onClick={(event) => {
+                     const input = event.currentTarget.previousSibling;
+                     input.value = Number(input.value) + 1;
                   }}
                >
                   +
